f0efedb26c1c837858eba0677ffc210003c7a128
matias <matiasadrianbacher.2006@gmail.com>
2026-02-02 17:19:43 -0300

    add yazi conf

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,3 +1,3 @@
-abbr -a --position anywhere --set-cursor='%' -- cod 'sg/codificacion%'
+abbr -a --position anywhere --set-cursor='%' -- cod '~/sg/codificacion%'
 abbr -a -- pms 'sudo pacman -S'
-abbr -a --position anywhere --set-cursor='%' -- .c '.config/%'
+abbr -a --position anywhere --set-cursor='%' -- .c '~/.config/%'
